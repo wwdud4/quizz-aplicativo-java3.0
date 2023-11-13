7a94271816180248281a0d233dc3e3ad3ec8ffcd
wwdud4 <141182636+wwdud4@users.noreply.github.com>
2023-11-13 10:16:54 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -52,7 +52,7 @@ let questions = [
       "Saturno",
       "Vênus",
       "Marte",
-      "jupter"
+      "júpiter"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
