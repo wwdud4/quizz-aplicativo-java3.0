effcfa95c63b06acf003d70e20c12afa4aaf36bc
wwdud4 <141182636+wwdud4@users.noreply.github.com>
2023-11-13 10:00:56 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,75 +1,72 @@
-// creating an array and passing the number, questions, options, and
-answers
+// creating an array and passing the number, questions, options, and answers
 let questions = [
-{
-numb: 1,
-question: "Quem foi o criador da obra de arte Mona Lisa?",
-answer: "Leonardo da vinci",
-options: [
-"picasso",
-"Michelangelo",
-"Rafael",
-"Leonardo da Vinci"
-]
-},
-{
-numb: 2,
-question: "qual é o MENOR país do mundo?",
-answer: "Vaticano",
-options: [
-"Japão",
-"Vaticano",
-"Portugal",
-"Inglaterra"
-]
-},
-{
-numb: 3,
-question: "De onde é a invenção do chuveiro elétrico?",
-answer: "brasil",
-options: [
-"França",
-"Australia",
-"Brasil",
-"China"
-]
-},
-{
-numb: 4,
-question: "Qual o livro mais vendido no mundo a seguir à Bíblia?",
-answer: "Dom Quixote",
-options: [
-"Dom Quixote",
-"O Pequeno Príncipe",
-"O Senhor dos Anéis",
-"Ela, a Feiticeira"
-]
+  {
+    numb: 1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Curiee",
+    options: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual o país mais novo do mundo?",
+    answer: "Sudão do Sul",
+    options: [
+      "Timor Lestet",
+      "Montenegro",
+      "Sudão do Sul",
+      "Kosovo"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
+    answer: "Solstício de verão",
+    options: [
+      "Equinócio de outono",
+      "Solstício de inverno",
+      "Solstício de verão",
+      "Solstício de outono"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
+    answer: "12",
+    options: [
+      "12",
+      "11",
+      "14",
+      "13"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
+    answer: "Vênus e Urano",
+    options: [
+      "Saturno e Júpiter",
+      "Vênus e Urano",
+      "Terra e Marte",
+      "Mercúrio e Saturno"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-},
-{
-numb: 5,
-question: "Quanto tempo a luz do Sol demora para chegar à Terra?",
-answer: "8 minutos",
-options: [
-"8 minutos",
-"3 minutos",
-"2 horas",
-"8 horas"
-]
-},
-// you can uncomment the below codes and make duplicate as more as you
-want to add question
-// but remember you need to give the numb value serialize like
-1,2,3,5,6,7,8,9.....
-// {
-// numb: 6,
-// question: "Your Question is Here",
-// answer: "Correct answer of the question is here",
-// options: [
-// "Option 1",
-// "option 2",
-// "option 3",
-// "option 4"
-// ]
-// },
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
